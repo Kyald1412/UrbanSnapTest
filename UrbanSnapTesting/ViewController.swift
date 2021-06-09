c2e1a0d3537c98dd23ac3c2eaf0285ef72379e81
Dhiky Aldwiansyah <dhikyaldwiansyah@gmail.com>
2021-06-09 12:06:40 +0700

    Initial Commit

---
+++ UrbanSnapTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UrbanSnapTesting
+//
+//  Created by Dhiky Aldwiansyah on 09/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
